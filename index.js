0aef6b72d6e26540a2343c6b30fb0d6d40c4666b
Avneesh Dev <avneesh.dev@gmail.com>
2023-03-11 09:01:46 -0500

    arrays lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = [
+    "Milo", "Otis", "Garfield"]
+    console.log(cats)
+function destructivelyAppendCat(name) {
+    return cats.push(name)
+}
+function destructivelyPrependCat(name) {
+    return cats.unshift(name)
+}
+function destructivelyRemoveLastCat() {
+return  cats.pop()
+}
+function destructivelyRemoveFirstCat() {
+    return cats.shift()
+}
+function appendCat(name){
+    const Cats = cats.slice() 
+    Cats.push(name) 
+    return Cats}
+
+    function prependCat(name){
+        const Cats = cats.slice() 
+        Cats.unshift(name) 
+        return Cats}
+
+        function removeLastCat(name){
+            const Cats = cats.slice()
+            Cats.pop(name);
+            return Cats
+        }
+        function removeFirstCat(name){
+            const Cats = cats.slice()
+            Cats.shift(name)
+            return Cats
+        }
